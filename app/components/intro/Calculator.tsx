5e4afc1ece16b3adebd07dd8a7a992179b6a71cc
maedeh <maedeh.a7879@gmail.com>
2025-01-07 09:00:54 +0330

    calcu doone

--- app/components/intro/Calculator.tsx
+++ app/components/intro/Calculator.tsx
@@ -13,37 +13,49 @@ function Calculator() {
         <div className="bg-secondary700 p-6 rounded-lg shadow-md">
           <h2 className="font-semibold mb-4 text-dark text-xs">جهت محاسبه نمره لطفا موارد زیر را تکمیل نمایید</h2>
           
-         <div className="grid grid-cols-2">
-            <div className=" flex  flex-col">
+         <div className="grid grid-cols-2 gap-4">
+          <div className="col-span-1">
+            
+          <div className=" flex  flex-col">
                 <div className="flex  gap-2">
-                  <label className="text-dark whitespace-nowrap text-xs mb-2">پاسخ صحیح</label>
+                  <label className="text-dark whitespace-nowrap text-xs mb-2 min-w-16">پاسخ صحیح</label>
                   <input type="number" className="w-full p-2 border border-gray-300 rounded-lg" placeholder="1 2"/>
                 </div>
                 
                 <div className="flex  gap-2">
-                  <label className="text-dark whitespace-nowrap text-xs mb-2">پاسخ غلط</label>
+                  <label className="text-dark whitespace-nowrap text-xs mb-2 min-w-16">پاسخ غلط</label>
                   <input type="number" className="w-full p-2 border border-gray-300 rounded-lg" placeholder="1 3"/>
                 </div>
                 
                 <div className="flex  gap-2">
-                  <label className="text-dark whitespace-nowrap text-xs mb-2">حذفی</label>
+                  <label className="text-dark whitespace-nowrap text-xs mb-2 min-w-16">حذفی</label>
                   <input type="number" className="w-full p-2 border border-gray-300 rounded-lg" placeholder="9 8 5"/>
                 </div>
               </div>
-              <div className=" flex flex-col gap-4">
-            <button className="bg-secondary900 flex gap-1 items-center text-white px-4 py-2 hover:bg-secondary800 text-xs rounded-2xl whitespace-nowrap">محاسبه نمره
-              <Image
+          </div>
+          <div className="col-span-1">
+          <div className=" flex flex-col gap-4">
+            <button className="bg-secondary900 flex gap-1 items-center text-white px-4 py-2 hover:bg-secondary800 text-xs rounded-2xl whitespace-nowrap  justify-center   ">
+            <Image
                         src={Processor}
                         alt="Processor icon"
-                        width={34}
-                        height={34} />
+                        width={24}
+                        height={24} />
+              محاسبه نمره
+            
               </button>
-              <div className="">
-                <span className="">
+              <div className="flex flex-col items-center gap-2">
+                <span className="text-xs text-dark">
+                نمره اکتسابی شما:
+                </span>
+                <span className="Ttext-white text-2xl bg-did  rounded-2xl font-medium px-6     py-1">
                   99.8
                 </span>
               </div>
               </div>
+          </div>
+
+
          </div>
         </div>
       
