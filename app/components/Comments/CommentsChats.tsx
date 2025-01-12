e5909c22b2ad2168d6c6dea3bd346cbdaf92b34e
maedeh <maedeh.a7879@gmail.com>
2025-01-12 16:55:04 +0330

    comment  fix

--- app/components/Comments/CommentsChats.tsx
+++ app/components/Comments/CommentsChats.tsx
@@ -1,19 +1,21 @@
 import Image from 'next/image'
 import ProfileDemo from '@/app/components/icons/profile-demo.svg'
+import User2 from '@/public/img/user2.png'
+
 
 function CommentsChats() {
   return (
     <>
-    <div className="bg-white px-8 py-6 rounded-2xl border border-did/15 shadow-custom-shadow flex flex-col gap-2">
+    <div className="bg-white px-8 py-6 rounded-2xl border border-did/15 shadow-custom-shadow flex flex-col gap-2 my-4">
         <div className="flex flex-col items-start gap-2 mb-4">
             <span className="text-dark text-xl font-bold">
             185 دیدگاه
             </span>
             <p className="text-secondary text-sm">به گفتگوی ما بپیوندید و نظرتان را با ما به اشتراک بگذارید ...</p>
         </div>
-        <div className="">
-        <div className="flex items-center justify-start gap-2">
-                <div className="w-full">
+        <div className="flex flex-col items-start gap-4 mb-8">
+            <div className="flex items-center justify-start gap-2">
+                <div className="">
                     <Image
                         src={ProfileDemo}
                         alt="CheckSmall icone"
@@ -21,14 +23,45 @@ function CommentsChats() {
                         width={80}
                         />
                 </div>
-               <div className="flex flex-col items-center justify-center">
-                <span className="text-dark text-base font-bold whitespace-nowrap">
+                <div className="flex flex-col items-center justify-center">
+                    <span className="text-secondary text-base font-bold whitespace-nowrap">
                     کاربر بیت گرف
                     </span>
-                    <span className="text-secondary text-xs whitespace-nowrap">
+                    <span className="text-secondary900 text-xs whitespace-nowrap">
                     1402/03/25
                     </span>
-               </div>
+                </div>
+            </div>
+            <div className="bg-did/15 rounded-3xl rounded-ss-none ms-11 px-6 py-4">
+                    <p className="text-dark text-base">
+                        سلام وقتتون بخیر<br /> میخواستم بدونم با وجود وضع فعلی اینترنت و محدودیت ها ، همچنان امکان درامدزایی از سایت های خارجی و پروژه گرفتن هست؟
+                    </p>
+            </div>
+        </div>
+
+        <div className="flex flex-col items-start gap-4 mb-8">
+            <div className="flex items-center justify-start gap-2">
+                <div className="rounded-full">
+                    <Image
+                        src={User2}
+                        alt="CheckSmall icone"
+                        height={80} 
+                        width={80}
+                        />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <span className="text-secondary text-base font-bold whitespace-nowrap">
+                    کاربر بیت گرف
+                    </span>
+                    <span className="text-secondary900 text-xs whitespace-nowrap">
+                    1402/03/25
+                    </span>
+                </div>
+            </div>
+            <div className="bg-did/15 rounded-3xl rounded-ss-none ms-11 px-6 py-4">
+                    <p className="text-dark text-base">
+                        سلام وقتتون بخیر<br /> میخواستم بدونم با وجود وضع فعلی اینترنت و محدودیت ها ، همچنان امکان درامدزایی از سایت های خارجی و پروژه گرفتن هست؟
+                    </p>
             </div>
         </div>
     </div>
